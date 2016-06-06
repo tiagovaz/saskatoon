2788bfcfb40392b4443cb7d8a08e4910ea21cc3e
Tiago Bortoletto Vaz <tiago@acaia.ca>
2016-06-06 05:59:33 -0400

    Improve properties template.

--- saskatoon/member/models.py
+++ saskatoon/member/models.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from django.db import models
+from django.template.defaultfilters import register
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
 from django.contrib.auth.models import User
