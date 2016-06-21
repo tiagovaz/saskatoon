c0863752def29a1917d0353e48aad765f9a87e64
Tassia Camoes Araujo <tassia@riseup.net>
2016-06-21 01:52:05 -0400

    Improve Harvest and Property models. Closes #56.

--- saskatoon/harvest/filters.py
+++ saskatoon/harvest/filters.py
@@ -4,4 +4,4 @@ from models import Harvest
 class HarvestFilter(django_filters.FilterSet):
     class Meta:
         model = Harvest
-        fields = ['status', 'pick_leader', 'is_active']
+        fields = ['status', 'pick_leader']
