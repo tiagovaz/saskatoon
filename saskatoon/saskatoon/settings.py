77a259b1cdc5b46cad8975ffd086c053a634381e
Tiago Bortoletto Vaz <tiago@debian.org>
2016-06-23 12:09:29 -0400

    Use Mysql.

--- saskatoon/saskatoon/settings.py
+++ saskatoon/saskatoon/settings.py
@@ -93,26 +93,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'saskatoon.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'saskatoon',
-#        'USER': '',
-#        'PASSWORD': '',
-#    }
-#}
+# USE MySQL for your local tests
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
