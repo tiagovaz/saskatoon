ef8861c5d604ce9b4cb7e15bebad51e41e8dd163
Tiago Bortoletto Vaz <tiago@acaia.ca>
2016-06-24 20:51:37 -0400

    Remove custom form from admin.

--- saskatoon/harvest/admin.py
+++ saskatoon/harvest/admin.py
@@ -35,6 +35,7 @@ class HarvestYieldInline(admin.TabularInline):
 
 class HarvestAdmin(admin.ModelAdmin):
     #form = HarvestForm
+    model = Harvest
     inlines = (PersonInline, HarvestYieldInline)
 
 
