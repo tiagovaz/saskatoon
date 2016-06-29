tiagovaz/saskatoon

commit 70dd2fbb530d476983ae61740505142b65b931ce

Fixed auth_user print.

saskatoon/member/models.py
@@ -56,7 +56,7 @@ class AuthUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         try:
-            return u"%s" % (self.person.__str__())
+            return u"%s" % (self.person)
         except:
        	    return self.email
 
@@ -72,11 +72,12 @@ class Actor(models.Model):
         verbose_name_plural = _("actors")
 
     def __str__(self):
+        #FIXME: much simplier as auth_user is now, no need to decode utf. change it when unit tests are available. 
         try:
-            person = '%s' % (self.person.__str__())
+            person = u'%s' % (self.person.__str__())
             return person.decode('utf8')
         except Person.DoesNotExist:
-            organization = '%s' % (self.organization.__str__())
+            organization = u'%s' % (self.organization.__str__())
             # if it is not a person it must be an organization
             return organization.decode('utf8')
 

commit 5ce63dc36ad91a8e816991fdc757a95eae559c7b

Fixed bug introduced by commit 70dd2fbb530d476983ae61740505142b65b931ce

saskatoon/member/models.py
@@ -72,14 +72,11 @@ class Actor(models.Model):
         verbose_name_plural = _("actors")
 
     def __str__(self):
-        #FIXME: much simplier as auth_user is now, no need to decode utf. change it when unit tests are available. 
         try:
-            person = u'%s' % (self.person.__str__())
-            return person.decode('utf8')
+            return u"%s" % (self.person)
         except Person.DoesNotExist:
-            organization = u'%s' % (self.organization.__str__())
             # if it is not a person it must be an organization
-            return organization.decode('utf8')
+            return u"%s" % (self.organization)
 
 
 @python_2_unicode_compatible