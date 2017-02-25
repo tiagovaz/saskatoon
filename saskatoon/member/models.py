e1ff2508b0f323dcec317897de1aec1d3f3d8d04
RignonNoel <rignon.noel@openmailbox.org>
2017-02-25 14:46:59 -0500

    refactor AuthUser to be able to use createsuperuser

--- saskatoon/member/models.py
+++ saskatoon/member/models.py
@@ -24,19 +24,19 @@ NOTIFICATION_TYPE_CHOICES = (
 
 
 class AuthUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None):
+    def create_user(self, email, password=None):
         if not email:
             raise ValueError('Users must have an email address')
 
-        user = self.model(username=username, email=self.normalize_email(email),
+        user = self.model(email=self.normalize_email(email),
                           )
         user.is_active = True
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password):
-        user = self.create_user(username=username, email=email, password=password)
+    def create_superuser(self, email, password):
+        user = self.create_user(email=email, password=password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
