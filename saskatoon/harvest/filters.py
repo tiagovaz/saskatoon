b8559e5a5a86282ca61d98f0d6e7aa380c5310b9
Tiago Bortoletto Vaz <tiago@acaia.ca>
2017-06-16 16:12:51 -0400

    Fix season filter.

--- saskatoon/harvest/filters.py
+++ saskatoon/harvest/filters.py
@@ -1,39 +1,29 @@
-from harvest.models import Harvest, HARVESTS_STATUS_CHOICES
-from member.models import AuthUser, Neighborhood
+from django.utils.translation import ugettext_lazy as _
 from django_filters import FilterSet, ChoiceFilter, ModelChoiceFilter
+from harvest.models import Harvest, HARVESTS_STATUS_CHOICES
+from member.models import AuthUser
 
 FILTER_HARVEST_CHOICES = list(HARVESTS_STATUS_CHOICES)
 FILTER_HARVEST_CHOICES.insert(0, ('', '---------'))
 
-
 class HarvestFilter(FilterSet):
     seasons = []
-
-    class Meta:
-        model = Harvest
-        fields = {
-            'status': ['exact'],
-            'pick_leader': ['exact'],
-            'trees': ['exact'],
-            'property__neighborhood': ['exact'],
-            'start_date': ['exact']
-        }
-
-    def __init__(self, *args, **kwargs):
-        """Return a list of years based on past and
-        current harvests dates"""
-
-        super(HarvestFilter, self).__init__(*args, **kwargs)
-        seasons = []
-        for y in Harvest.objects.all():
-            if y.start_date is not None:
-                t_seasons = (
+    for y in Harvest.objects.all():
+        if y.start_date is not None:
+            t_seasons = (
                     y.start_date.strftime("%Y"),
                     y.start_date.strftime("%Y")
                 )
-                seasons.append(t_seasons)
-        seasons.insert(0, ('', '---------'))
-        self.seasons = seasons
+            seasons.append(t_seasons)
+    seasons.insert(0, ('', '---------'))
+
+    seasons = list(set(seasons))
+
+    start_date = ChoiceFilter(
+        choices=seasons,
+        label=_("Season"),
+        lookup_expr='year'
+    )
 
     status = ChoiceFilter(
         choices=FILTER_HARVEST_CHOICES
@@ -46,8 +36,12 @@ class HarvestFilter(FilterSet):
         required=False
     )
 
-    start_date = ChoiceFilter(
-        choices=seasons,
-        label="Season",
-        lookup_expr='year'
-    )
+    class Meta:
+        model = Harvest
+        fields = {
+        'status': ['exact'],
+        'pick_leader': ['exact'],
+        'trees': ['exact'],
+        'property__neighborhood': ['exact'],
+        'start_date': ['exact'],
+        }
