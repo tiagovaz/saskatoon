c7e43d15c39b9550dae99f6e4b29eb4eb9c69e0d
root <root@08efd375aeba>
2018-05-30 15:27:08 +0000

    Update translation py files.

--- saskatoon/harvest/translation.py
+++ saskatoon/harvest/translation.py
@@ -2,16 +2,16 @@ from modeltranslation.translator import register, TranslationOptions
 from harvest.models import Equipment, EquipmentType, TreeType
 
 
-#@register(Equipment)
-#class EquipmentTranslationOptions(TranslationOptions):
-#    fields = ['description']
-#
-#
-#@register(EquipmentType)
-#class EquipmentTypeTranslationOptions(TranslationOptions):
-#    fields = ['name']
-#
-#
-#@register(TreeType)
-#class TreeTypeTranslationOptions(TranslationOptions):
-#    fields = ['name', 'fruit_name']
+@register(Equipment)
+class EquipmentTranslationOptions(TranslationOptions):
+    fields = ['description']
+
+
+@register(EquipmentType)
+class EquipmentTypeTranslationOptions(TranslationOptions):
+    fields = ['name']
+
+
+@register(TreeType)
+class TreeTypeTranslationOptions(TranslationOptions):
+    fields = ['name', 'fruit_name']
