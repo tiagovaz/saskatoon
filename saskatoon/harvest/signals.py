88519ceba21225946fcee1800a17ba2838c01f1d
Tassia Camoes Araujo <tassia@acaia.ca>
2019-05-20 04:36:05 -0300

    Fixed bug on sending email to a "None" pick_leader. First check if pick_leader, only then check the author of the comment and attempts to send the email.

--- saskatoon/harvest/signals.py
+++ saskatoon/harvest/signals.py
@@ -39,32 +39,34 @@ def changed_by(sender, instance, **kwargs):
 def comment_send_mail(sender, instance, **kwargs):
     current_request = CrequestMiddleware.get_request()
 
-    # Send email only if comment comes from someone else
-    if instance.author.email != instance.harvest.pick_leader.email:
-        # Building email content
-        pick_leader_email = list()
-        pick_leader_email.append(instance.harvest.pick_leader.email)
-        pick_leader_name = instance.harvest.pick_leader.person.first_name
-        mail_subject = u"New comment from %s" % instance.author
-        message = u'Hi %s, \n\n' \
-                  u'On %s %s left the following comment\n' \
-                  u'in the harvest "%s":\n\n' \
-                  u'%s\n\n' \
-                  u'You can see all comments related to this ' \
-                  u'harvest at\n' \
-                  u'http://saskatoon.lesfruitsdefendus.org/harvest/%s.' \
-                  u'\n\n' \
-                  u'Yours,\n' \
-                  u'--\n' \
-                  u'Saskatoon Harvest System' % \
-                  (
-                      pick_leader_name,
-                      instance.created_date.strftime('%b %d at %H:%M'),
-                      instance.author,
-                      instance.harvest.property.publishable_location,
-                      instance.content,
-                      instance.harvest.id
-                  )
+    # First check if pick_leader is set
+    if instance.harvest.pick_leader:
+        # Send email only if comment comes from someone else
+        if instance.author.email != instance.harvest.pick_leader.email:
+            # Building email content
+            pick_leader_email = list()
+            pick_leader_email.append(instance.harvest.pick_leader.email)
+            pick_leader_name = instance.harvest.pick_leader.person.first_name
+            mail_subject = u"New comment from %s" % instance.author
+            message = u'Hi %s, \n\n' \
+                      u'On %s %s left the following comment\n' \
+                      u'in the harvest "%s":\n\n' \
+                      u'%s\n\n' \
+                      u'You can see all comments related to this ' \
+                      u'harvest at\n' \
+                      u'http://saskatoon.lesfruitsdefendus.org/harvest/%s.' \
+                      u'\n\n' \
+                      u'Yours,\n' \
+                      u'--\n' \
+                      u'Saskatoon Harvest System' % \
+                      (
+                          pick_leader_name,
+                          instance.created_date.strftime('%b %d at %H:%M'),
+                          instance.author,
+                          instance.harvest.property.publishable_location,
+                          instance.content,
+                          instance.harvest.id
+                      )
 
-        # Sending email to pick leader
-        _send_mail(mail_subject, message, pick_leader_email)
+            # Sending email to pick leader
+            _send_mail(mail_subject, message, pick_leader_email)
