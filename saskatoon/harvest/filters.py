4c32b1be55d921aba2b4623108a3b4bc74d2e9c2
Tiago Bortoletto Vaz <tiago@acaia.ca>
2019-06-14 17:53:45 +0000

    Fix properties filter order

--- saskatoon/harvest/filters.py
+++ saskatoon/harvest/filters.py
@@ -85,4 +85,4 @@ class PropertyFilter(FilterSet):
 
     class Meta:
         model = Property
-        fields = ['is_active', 'authorized', 'neighborhood', 'trees', 'ladder_available', 'ladder_available_for_outside_picks', 'pending']
+        fields = ['is_active', 'authorized', 'neighborhood', 'trees', 'pending', 'ladder_available', 'ladder_available_for_outside_picks']
